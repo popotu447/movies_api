6d8c1b5e4f6e7750c1f06ca94c04c81be5f2798f
popotu447 <my_sql@op.pl>
2025-05-29 14:38:36 +0200

    ip change to public

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def delete_movie(movie_id):
     return jsonify({'message': 'Movie deleted'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
